b6cca89de8a0699bea142b244c711e8140ab74ca
Mishieck Mwale <57598264+Mishieck@users.noreply.github.com>
2022-01-05 19:40:24 +0200

    feat: use icon as is in props in ContactLink

--- src/components/header/contact-link/contact-link.ts
+++ src/components/header/contact-link/contact-link.ts
@@ -9,8 +9,6 @@ type Props = {
 };
 
 export const contactLink = async (props: Props) => {
-  props.icon = `/src/assets/images/contacts/${props.icon}.svg`;
-
   return $create({
     id: "mishieck-github-io-contact-link",
     props,
